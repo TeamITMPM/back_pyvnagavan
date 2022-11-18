f74cbd5952c5902406eae6335735195703a042bc
makkob <maximkabig945@gmail.com>
2022-11-18 22:29:53 +0200

    sq

--- db.js
+++ db.js
@@ -1,2 +1,12 @@
 const { Sequelize } = require("sequelize");
-module.express = new Sequelize();
+
+module.express = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mariadb",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
