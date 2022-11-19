6cc19ce1f2daf5ce2a1752fcaa58ad245eb9efcb
makkob <maximkabig945@gmail.com>
2022-11-19 14:50:05 +0200

    db

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
-module.express = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "mariadb",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  }
+module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: 'mariadb',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+    },
 );
--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-require("dotenv").config();
-const express = require("express");
-const sequelize = require("./db");
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 const app = express();
 
 const start = async () => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    app.listen(PORT, () =>
-      console.log(`Server fucking started on port ${PORT}`)
-    );
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () =>
+            console.log(`Server fucking started on port ${PORT}`),
+        );
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 start();
-
-console.log("sequelize", sequelize);
