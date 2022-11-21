89d23fe7dae6d52216b2387a6bbe22beddb2e309
makkob <maximkabig945@gmail.com>
2022-11-21 22:46:55 +0200

    r

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5002;
 
 const app = express();
+// что б отправлять запросы с брвузера
+app.use(cors());
+// что б приложение могло парсить json формат
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Воно працює=)' });
+});
 
 const start = async () => {
     try {
