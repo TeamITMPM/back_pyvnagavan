9d036bbf1507cc954db312ff29599777c007ff05
makkob <maximkabig945@gmail.com>
2022-11-23 01:53:17 +0200

    routOK

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 // что б приложение могло парсить json формат
 app.use(express.json());
-app.use('./api', router);
+app.use('/api', router);
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Воно працює=)' });
