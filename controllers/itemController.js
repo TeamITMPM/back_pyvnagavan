4e41e07c26b0f2e0d3fa700216dc97190690372c
makkob <maximkabig945@gmail.com>
2022-11-26 01:39:04 +0200

    it

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,12 +1,27 @@
 const uuid = require('uuid');
 const path = require('path');
+const { Item } = require('../models/models');
+const { nextTick } = require('process');
+const ApiError = require('../error/ApiError');
 
 class ItemController {
-    async create(req, res) {
-        const { name, price, brandId, typeId, info } = req.body;
-        const { img } = req.files;
-        let filename = uuid.v4() + '.jpg';
-        img.mv(path.resolve(__dirname, '..', 'static', fileName));
+    async create(req, res, next) {
+        try {
+            const { name, price, brandId, typeId, info } = req.body;
+            const { img } = req.files;
+            let filename = uuid.v4() + '.jpg';
+            img.mv(path.resolve(__dirname, '..', 'static', fileName));
+            const item = await Item.create({
+                name,
+                price,
+                brandId,
+                typeId,
+                img: fileName,
+            });
+            return res.json(item);
+        } catch (e) {
+            next(ApiError.badRequest(e.message));
+        }
     }
     async getAll(req, res) {}
     async getOne(req, res) {}
