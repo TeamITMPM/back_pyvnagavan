20247ad041d9bb7592318e47eb3c89c2d499d702
makkob <maximkabig945@gmail.com>
2022-11-26 01:33:06 +0200

    it

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,10 +1,12 @@
 const uuid = require('uuid');
+const path = require('path');
 
 class ItemController {
     async create(req, res) {
         const { name, price, brandId, typeId, info } = req.body;
         const { img } = req.files;
         let filename = uuid.v4() + '.jpg';
+        img.mv(path.resolve(__dirname, '..', 'static', fileName));
     }
     async getAll(req, res) {}
     async getOne(req, res) {}
