240499cef6ac17c000f36af5ae0d2109dbfaab3e
makkob <maximkabig945@gmail.com>
2022-11-28 14:45:24 +0200

    jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,31 @@
 const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const JWT = require('jsonwebtoken');
+const { User, Basket } = require('../models/models');
+
 class UserController {
-    async registration(req, res) {}
+    async registration(req, res, next) {
+        const { email, password, role } = req.body;
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некоректний email або password'));
+        }
+        const candidate = await User.findOne({ where: { email } });
+        if (candidate) {
+            return next(
+                ApiError.badRequest('Користувач із таким email вже існує'),
+            );
+        }
+        // хешируем пароль
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({ email, role, password: hashPassword });
+        const basket = await Basket.create({ userId: user.id });
+    }
+
     async login(req, res) {}
     async check(req, res, next) {
         const { id } = req.query;
         if (!id) {
-            return next(ApiError.badRequest('Не задан ID'));
+            return next(ApiError.badRequest('Не заданий ID'));
         }
         res.json(id);
     }
