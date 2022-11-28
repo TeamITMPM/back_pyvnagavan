546189c8d6c2625523c6cfe04bde2af18e40bcda
makkob <maximkabig945@gmail.com>
2022-11-29 00:29:15 +0200

    reg

--- models/models.js
+++ models/models.js
@@ -6,13 +6,13 @@ const User = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     email: { type: DataTypes.STRING, unique: true },
     password: { type: DataTypes.STRING },
-    role: { type: DataTypes.STRING, defaultValue: 'USER' },
     phone: { type: DataTypes.STRING },
-    discount: { type: DataTypes.TINYINT, defaultValue: 0 },
     firstName: { type: DataTypes.STRING },
     secondName: { type: DataTypes.STRING },
-    clientRating: { type: DataTypes.TINYINT, defaultValue: 0 },
     dateOfBirthsday: { type: DataTypes.DATEONLY },
+    clientRating: { type: DataTypes.TINYINT, defaultValue: 0 },
+    discount: { type: DataTypes.TINYINT, defaultValue: 0 },
+    role: { type: DataTypes.STRING, defaultValue: 'USER' },
 });
 
 const Basket = sequelize.define('basket', {
